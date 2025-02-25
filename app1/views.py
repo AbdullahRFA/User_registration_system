71bb4a6635f622bc99d600668103529f7fb2c857
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-25 23:48:31 +0600

    final touch

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, HttpResponse,redirect
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import HttpResponse, redirect, render
 
 # Create your views here.
 

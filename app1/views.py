d0269c2ad37032efa0da0bd17c97967224431327
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-25 22:30:54 +0600

    signup method complete

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def HomePage(request):
@@ -12,6 +13,14 @@ def SignUp(request):
         email = request.POST.get("email")
         password1 = request.POST.get("password1")
         password2 = request.POST.get("password2")
+        
+        if password1 != password2:
+            return HttpResponse("Your password and confirm password does not match")
+        else:
+            my_user = User.objects.create_user(username,email,password1)
+            my_user.save()
+        if my_user:
+            return redirect('login')
         
         print(username,email,password1,password2)
     return render(request,'signup.html')

6be32d865de34db39d953c9e2346675cb5da00fc
AbdullahRFA <shakibrybmn@gmail.com>
2025-02-25 22:50:31 +0600

    added login and logout method

--- app1/views.py
+++ app1/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, HttpResponse,redirect
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate,login
 # Create your views here.
 def HomePage(request):
     return render(request,'home.html')
 def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('pass')
+        
+        user = authenticate(request, username=username,password=password)
+        if user is None:
+            return HttpResponse("Username or password is incorrect")
+        
+        login(request,user)
+        return redirect('homepage')
+        
+        
     return render(request,'login.html')
 
 def SignUp(request):
